14772d1530de35f036bdf49aa79dc1be0e9313f1
Kib-Golgit <kibatka.golgit@gmail.com>
2024-07-26 23:30:42 +0100

    Add files via upload

---
+++ store/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", )()
+=> {
+    const products=[
+        { type:"shoes", name:"shoe1",
+            price:"#35000"
+        },
+        { type:"shoes", name:"shoe2",
+            price:"#35000"
+        }
+    ]
+}
